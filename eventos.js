b737ff620767849db2055ccdcb7bbb380df46729
Magalí Díaz <yajaira281096@gmail.com>
2024-08-25 00:51:09 -0300

    Agregue el archivo eventos.js , cree un div que contenga al button, le di estilo al div y le agregue un manejador de eventos al div para que al darle click se muestre una alerta con Hola soy el div

---
+++ eventos.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    const buttonContainer = document.querySelector('.button-container');
+        buttonContainer.addEventListener('click', function() {
+            alert('¡Hola! Soy el div');
+        }
+    
+    )
+});
